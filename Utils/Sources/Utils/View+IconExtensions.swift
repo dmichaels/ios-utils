3e7cc80832bae4a3fd79abbfdae1828278285137
David Michaels <dmichaels@gmail.com>
2025-06-10 18:52:01 -0400

    etc

---
+++ Utils/Sources/Utils/View+IconExtensions.swift
@@ -0,0 +1,14 @@
+import AVFoundation
+
+struct ColorCircleIcon: View {
+    var body: some View {
+        Circle()
+            .fill(
+                AngularGradient(
+                    gradient: Gradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .red]),
+                    center: .center
+                )
+            )
+            .frame(width: 24, height: 24)
+    }
+}
