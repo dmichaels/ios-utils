bbd6cd5308ffc9ea32f631b680039f35a13dfc64
David Michaels <dmichaels@gmail.com>
2025-07-02 08:56:50 -0400

    etc

--- Utils/Sources/Utils/IconLabel.swift
+++ Utils/Sources/Utils/IconLabel.swift
@@ -4,21 +4,26 @@ public struct IconLabel: View {
     private var _text: String
     private var _icon: String
     private var _iconWidth: CGFloat
-    public init(_ text: String, _ icon: String, iconWidth: CGFloat = 32.0) {
+    private var _compact: Bool
+    public init(_ text: String, _ icon: String, iconWidth: CGFloat = 32.0, compact: Bool = true) {
         self._text = text
         self._icon = icon
         self._iconWidth = iconWidth
+        self._compact = compact
     }
     public var body: some View {
         HStack(alignment: .center, spacing: 8) {
             if (self._icon == "COLOR") {
                 ColorCircleIcon().frame(width: self._iconWidth, alignment: .leading)
+                    .padding(.leading, self._compact ? -4 : 0)
             }
             else {
                 Image(systemName: self._icon).frame(width: self._iconWidth, alignment: .leading)
+                    .padding(.leading, self._compact ? -4 : 0)
             }
             Text(self._text)
                 .alignmentGuide(.leading) { d in d[.leading] }
+                .padding(.leading, self._compact ? -4 : 0)
                 .fixedSize()
                 .layoutPriority(1)
             Spacer()
