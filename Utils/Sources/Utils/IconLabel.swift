98565d2d46a78e6ea712f65b7973f58a4dd14ed3
David Michaels <dmichaels@gmail.com>
2025-07-04 08:06:15 -0400

    disable-icon-label

--- Utils/Sources/Utils/IconLabel.swift
+++ Utils/Sources/Utils/IconLabel.swift
@@ -5,6 +5,7 @@ public struct IconLabel: View {
     private var _icon: String
     private var _iconWidth: CGFloat
     private var _compact: Bool
+    @Environment(\.isEnabled) private var isEnabled: Bool
     public init(_ text: String, _ icon: String, iconWidth: CGFloat = 32.0, compact: Bool = true) {
         self._text = text
         self._icon = icon
@@ -16,16 +17,19 @@ public struct IconLabel: View {
             if (self._icon == "COLOR") {
                 ColorCircleIcon().frame(width: self._iconWidth, alignment: .leading)
                     .padding(.leading, self._compact ? -4 : 0)
+                    .foregroundColor(isEnabled ? .primary : .gray)
             }
             else {
                 Image(systemName: self._icon).frame(width: self._iconWidth, alignment: .leading)
                     .padding(.leading, self._compact ? -4 : 0)
+                    .foregroundColor(isEnabled ? .primary : .gray)
             }
             Text(self._text)
                 .alignmentGuide(.leading) { d in d[.leading] }
                 .padding(.leading, self._compact ? -4 : 0)
                 .fixedSize()
                 .layoutPriority(1)
+                .foregroundColor(isEnabled ? .primary : .gray)
             Spacer()
         }
     }
