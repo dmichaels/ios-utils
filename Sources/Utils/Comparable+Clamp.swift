fa80278bc43faa3ea622458c983c5a3604dd6ab7
David Michaels <dmichaels@gmail.com>
2025-08-11 10:01:29 -0400

    clamped

--- Sources/Utils/Comparable+Clamp.swift
+++ Sources/Utils/Comparable+Clamp.swift
@@ -3,3 +3,12 @@ extension Comparable {
         min(max(self, range.lowerBound), range.upperBound)
     }
 }
+
+extension BinaryFloatingPoint {
+    public func clamped(_ range: ClosedRange<Self>) -> Self {
+        min(max(self, range.lowerBound), range.upperBound)
+    }
+    public func clampedInt(_ range: ClosedRange<Self>) -> Int {
+        Int(min(max(self, range.lowerBound), range.upperBound))
+    }
+}
