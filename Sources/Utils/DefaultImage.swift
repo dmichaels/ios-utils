5f732837263ac5c18823cd1d55457a3be5811807
David Michaels <dmichaels@gmail.com>
2025-08-18 18:28:06 -0400

    DefaultImage-updates

--- Sources/Utils/DefaultImage.swift
+++ Sources/Utils/DefaultImage.swift
@@ -1,12 +1,23 @@
 import SwiftUI
 
 public class DefaultImage {
+
     public static let bitmapInfo: UInt32 = CGImageAlphaInfo.premultipliedLast.rawValue
     public static let space: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+
     public static let instance: CGImage = {
-        return CGContext(
-            data: nil, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4,
-            space: DefaultImage.space, bitmapInfo: DefaultImage.bitmapInfo
-        )!.makeImage()!
+        return DefaultImage.context(width: 1, height: 1).makeImage()!
     }()
+
+    public static func context(width: Int, height: Int) -> CGContext {
+        return CGContext(data: nil, width: width, height: height,
+                         bitsPerComponent: 8, bytesPerRow: width * Screen.channels,
+                         space: DefaultImage.space, bitmapInfo: DefaultImage.bitmapInfo)!
+    }
+
+    public static func context(width: Int, height: Int, buffer: UnsafeMutableRawPointer) -> CGContext {
+        return CGContext(data: buffer, width: width, height: height,
+                         bitsPerComponent: 8, bytesPerRow: width * Screen.channels,
+                         space: DefaultImage.space, bitmapInfo: DefaultImage.bitmapInfo)!
+    }
 }
