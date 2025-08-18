2e68bae661c3693e809502eb602eea606d352fc7
David Michaels <dmichaels@gmail.com>
2025-08-18 07:21:38 -0400

    etc

---
+++ Sources/Utils/DefaultImage.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public class DefaultImage {
+    public static let bitmapInfo: UInt32 = CGImageAlphaInfo.premultipliedLast.rawValue
+    public static let space: CGColorSpace? = CGColorSpaceCreateDeviceRGB()
+    public static let instance: CGImage = {
+        return CGContext(
+            data: nil, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4,
+            space: DefaultImage.space!, bitmapInfo: DefaultImage.bitmapInfo
+        )!.makeImage()!
+    }()
+}
