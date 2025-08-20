506e3f5c8e3865ce468d51884c4864cc01e87e97
David Michaels <dmichaels@gmail.com>
2025-08-19 23:28:01 -0400

    Display-changes

--- Sources/Utils/Display.swift
+++ Sources/Utils/Display.swift
@@ -1,36 +1,42 @@
 import SwiftUI
 
-@MainActor
-public enum Display
+public struct Display
 {
-    public static let channels: Int = 4
-    public static let scale: CGFloat = UIScreen.main.scale
-    public static let size: CGSize = UIScreen.main.bounds.size
-    public static let width: Int = Int(floor(Display.size.width))
-    public static let height: Int = Int(floor(Display.size.height))
+    public let channels: Int = 4
+    public let scale: CGFloat
+    public let size: CGSize
+    public let width: Int
+    public let height: Int
 
-    public static func scale(_ value: Int) -> Int { Int(round(CGFloat(value) * Display.scale)) }
-    public static func unscale(_ value: Int) -> Int { Int(round(CGFloat(value) / Display.scale)) }
+    public init(size: CGSize, scale: CGFloat) {
+        self.scale = scale
+        self.size = size
+        self.width = Int(floor(size.width))
+        self.height = Int(floor(size.height))
+    }
 
-    public static func scale(_ value: Int, scaling: Bool) -> Int { scaling ? Display.scale(value) : value }
-    public static func unscale(_ value: Int, scaling: Bool) -> Int { scaling ? Display.unscale(value) : value }
+    public func scale(_ value: Int) -> Int { Int(round(CGFloat(value) * self.scale)) }
+    public func unscale(_ value: Int) -> Int { Int(round(CGFloat(value) / self.scale)) }
+
+    public func scale(_ value: Int, scaling: Bool) -> Int { scaling ? self.scale(value) : value }
+    public func unscale(_ value: Int, scaling: Bool) -> Int { scaling ? self.unscale(value) : value }
 
     // Returns the scaled and unscaled values for the given value as a tuple (in that order),
     // based on whether or not the given value is scaled, and whether or not we (our caller) is
     // currently in "scaling mode" (i.e. whether or not we want a scaled value as the scaled result).
     //
-    public static func scaler(_ value: Int, scaled: Bool, scaling: Bool) -> (Int, Int) {
+    public func scaler(_ value: Int, scaled: Bool, scaling: Bool) -> (Int, Int) {
         if (scaled) {
             if (scaling) {
-                return (value, Display.unscale(value))
+                return (value, self.unscale(value))
             }
             else {
-                let unscaledValue: Int = Display.unscale(value)
+                let unscaledValue: Int = self.unscale(value)
                 return (unscaledValue, unscaledValue)
             }
         }
         else if (scaling) {
-            return (Display.scale(value), value)
+            return (self.scale(value), value)
         }
         else {
             return (value, value)
@@ -39,8 +45,8 @@ public enum Display
 
     // Same-as/uses above but assigns the scaled/unscaled values from the tuple to the given inout arguments.
     //
-    public static func scaler(_ value: Int, _ scaledValue: inout Int,
-                                            _ unscaledValue: inout Int, scaled: Bool, scaling: Bool) {
-        (scaledValue, unscaledValue) = Display.scaler(value, scaled: scaled, scaling: scaling)
+    public func scaler(_ value: Int, _ scaledValue: inout Int,
+                                     _ unscaledValue: inout Int, scaled: Bool, scaling: Bool) {
+        (scaledValue, unscaledValue) = self.scaler(value, scaled: scaled, scaling: scaling)
     }
 }
